654f42db6767e7c9f94cc69c51f049c5952b7fde
apa4336 <apa4336@gmail.com>
2022-01-19 15:35:29 +0800

    v1.0

---
+++ js/menu.js
@@ -0,0 +1,27 @@
+/**
+
+    <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.html">Home</a></li>
+    <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about.html">About</a></li>
+    <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.html">Products</a></li>
+    <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.html">Store</a></li>
+ */
+let meuuData = [
+    {
+        title:'Home',
+        url:'index.html'
+    },
+    {
+        title:'About',
+        url:'About.html'
+    },
+    {
+        title:'Products',
+        url:'products.html'
+    },
+    {
+        title:'store',
+        url:'store.html'
+    }
+];
+
+console.log();//Home
---
+++ js/menu1.js
@@ -0,0 +1,9 @@
+let menuHtml =`
+<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.html">Home</a></li>
+<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about.html">About</a></li>
+<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.html">Products</a></li>
+<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.html">Store</a></li>
+`;
+// 取得 DOM 的節點
+let menu = document.getElementById('menu');
+menu.textContent = menu
